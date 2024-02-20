a00e30297782a6b272300da60b00b639d1d3d113
den.darknezz@gmail.com <den.darknezz@gmail.com>
2024-02-20 23:49:44 +0500

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Math
+{
+    public class Matrix
+    {
+        private int matrixSize;
+        public int[,] matrix;   // матрица смежности
+
+        public Matrix()
+        {
+            matrixSize = 10;
+            matrix = new int[matrixSize, matrixSize];
+        }
+
+        public Matrix(int[,] matrix)
+        {
+            this.matrixSize = matrix.GetLength(0);
+            this.matrix = matrix;
+        }
+
+        /// <summary>
+        /// Количество компонентов связности
+        /// </summary>
+        /// <returns></returns>
+        public int GetCountComponents()
+        {
+            bool[] visited = new bool[matrixSize];
+            int countComponents = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                if (!visited[i])
+                {
+                    DFS(i, visited);
+                    countComponents++;
+                }
+            }
+
+            return countComponents;
+        }
+
+        /// <summary>
+        /// Обход в глубину
+        /// </summary>
+        /// <param name="vertex"></param>
+        /// <param name="visited"></param>
+        private void DFS(int vertex, bool[] visited) 
+        {
+            visited[vertex] = true;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                if (matrix[vertex, i] == 1 && !visited[i])
+                {
+                    DFS(i, visited);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Матрица достижимости
+        /// </summary>
+        /// <param name="adjacencyMatrix"></param>
+        /// <returns></returns>
+        public int[,] GetReachabilityMatrix()
+        {
+            int[,] reachabilityMatrix = new int[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    reachabilityMatrix[i, j] = matrix[i, j];
+
+                    //// у другого чувака
+                    //if (i == j)    
+                    //    matrixOfReachability[i, j] = 1;
+                    //else if (matrix[i, j] == 0)  
+                    //    matrixOfReachability[i, j] = 0;  
+                    //else
+                    //    matrixOfReachability[i, j] = 1;
+                    
+                }
+            }
+
+            for (int k = 0; k < matrixSize; k++)
+            {
+                for (int i = 0; i < matrixSize; i++)
+                {   
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        if (reachabilityMatrix[i, k] == 1 && reachabilityMatrix[k, j] == 1)
+                        {
+                            reachabilityMatrix[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+            return reachabilityMatrix;
+        }
+
+        public void PrintReachabilityMatrix()
+        {
+            Matrix Matrix = new Matrix(GetReachabilityMatrix());
+            Console.WriteLine($"Матрица достижимости:\n{Matrix}");
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int width = matrix.GetLength(1);
+            int height = matrix.GetLength(0);
+            stringBuilder.AppendLine("┌" + new string('─', width * 4 - 1) + "┐");
+            for (int i = 0; i < height; i++)
+            {
+                stringBuilder.Append("│ ");
+                for (int j = 0; j < width; j++)
+                {
+                    if (matrix[j, i] == int.MaxValue)
+                        stringBuilder.Append($"X │ ");
+                    else
+                        stringBuilder.Append($"{matrix[i, j]} │ ");
+                }
+                stringBuilder.AppendLine();
+
+                if (i == height - 1)
+                {
+                    stringBuilder.AppendLine("└" + new string('─', width * 4 - 1) + "┘");
+                }
+                else
+                {
+                    stringBuilder.AppendLine("├" + new string('─', width * 4 - 1) + "┤");
+                }
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Lab5Math
+{
+    public class Program
+    {
+        static int[,] ReadMatrixFromFile(string filename)
+        {
+            int[,] matrix = new int[10, 10];
+            string[] lines = File.ReadAllLines(filename);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] values = lines[i].Split(' ');
+                for (int j = 0; j < 10; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void Main()
+        {
+            var fileName = "g1";
+            for(int i = 1; i <= 4; i++)
+            {
+                var fileFullName = fileName + i + ".txt";
+                var matrix = new Matrix(ReadMatrixFromFile(fileFullName));
+
+                Console.WriteLine($"\t\t>>{fileFullName}<<");
+                Console.WriteLine("Компонента матрицы: " + matrix.GetCountComponents());
+                matrix.PrintReachabilityMatrix();
+            }
+        }
+    }
+}
